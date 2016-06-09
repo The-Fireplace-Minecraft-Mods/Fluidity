5f2f9dadd5e43d020bca1e8665dbc65dc4976fb6
The_Fireplace <the.f1repl4ce@gmail.com>
2016-06-09 17:27:40 -0500

    Star-Steel Anvil gradually regenerates. Closes #5

--- src/main/java/the_fireplace/fluidity/blocks/BaseMetalAnvil.java
+++ src/main/java/the_fireplace/fluidity/blocks/BaseMetalAnvil.java
@@ -2,8 +2,13 @@ package the_fireplace.fluidity.blocks;
 
 import cyano.basemetals.init.Materials;
 import cyano.basemetals.material.MetalMaterial;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import the_fireplace.fluidity.Fluidity;
 import the_fireplace.moreanvils.blocks.MaterialAnvil;
+
+import java.util.Random;
 
 /**
  * @author The_Fireplace
@@ -16,5 +21,14 @@ public class BaseMetalAnvil extends MaterialAnvil {
         setUnlocalizedName(material.getName()+"_anvil");
         setRegistryName(material.getName()+"_anvil");
         this.metalMaterial = material;
+        setTickRandomly(material == Materials.starsteel);
+    }
+
+    @Override
+    public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random random)
+    {
+        if(metalMaterial == Materials.starsteel && random.nextInt(20) == 1 && state.getValue(DAMAGE) > 0){
+            worldIn.setBlockState(pos, blockState.getBaseState().withProperty(FACING, state.getValue(FACING)).withProperty(DAMAGE, state.getValue(DAMAGE)-1));
+        }
     }
 }
