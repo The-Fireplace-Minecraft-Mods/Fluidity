ef0a5519146aea85a665c411963b0128c2f244da
The_Fireplace <the.f1repl4ce@gmail.com>
2016-06-10 21:23:27 -0500

    Efficiency improvement

--- src/main/java/the_fireplace/fluidity/proxy/BMICClient.java
+++ src/main/java/the_fireplace/fluidity/proxy/BMICClient.java
@@ -3,14 +3,12 @@ package the_fireplace.fluidity.proxy;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import the_fireplace.fluidity.compat.BaseMetalsIronChests;
 import the_fireplace.fluidity.entity.tile.renderer.TileEntityFluidityChestRenderer;
 import the_fireplace.fluidity.enums.BaseMetalsIronChestType;
-import the_fireplace.fluidity.events.BaseMetalsIronChestsClientEvents;
 
 /**
  * @author The_Fireplace
@@ -19,16 +17,12 @@ import the_fireplace.fluidity.events.BaseMetalsIronChestsClientEvents;
 public class BMICClient implements BMICProxy {
     public void register(){
         Item chestItem = Item.getItemFromBlock(BaseMetalsIronChests.fluidityChest);
-        BaseMetalsIronChestType[] chestTypes = BaseMetalsIronChestType.values();
-        int chestCount = chestTypes.length;
 
-        int i;
-        for(i = 0; i < chestCount; ++i) {
-            BaseMetalsIronChestType type = chestTypes[i];
+        for(BaseMetalsIronChestType type : BaseMetalsIronChestType.values()) {
+
             ModelLoader.setCustomModelResourceLocation(chestItem, type.ordinal(), new ModelResourceLocation(chestItem.getRegistryName(), "variant=" + type.getName()));
 
             ClientRegistry.bindTileEntitySpecialRenderer(type.clazz, new TileEntityFluidityChestRenderer());
         }
-        MinecraftForge.EVENT_BUS.register(new BaseMetalsIronChestsClientEvents());
     }
 }
