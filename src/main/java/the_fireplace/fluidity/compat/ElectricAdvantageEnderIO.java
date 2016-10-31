017918608774e2308bd73309cec47b7448a97c2a
The_Fireplace <the.f1repl4ce@gmail.com>
2016-10-31 13:24:20 -0500

    Update mod versions, fix crash with the latest EnderIO. Closes #19

--- src/main/java/the_fireplace/fluidity/compat/ElectricAdvantageEnderIO.java
+++ src/main/java/the_fireplace/fluidity/compat/ElectricAdvantageEnderIO.java
@@ -1,6 +1,6 @@
 package the_fireplace.fluidity.compat;
 
-import crazypants.enderio.EnderIO;
+import crazypants.enderio.ModObject;
 import crazypants.enderio.material.Material;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
@@ -16,8 +16,8 @@ public class ElectricAdvantageEnderIO implements IModCompat {
 
     @Override
     public void init() {
-        OreDictionary.registerOre("silicon", new ItemStack(EnderIO.itemMaterial, 1, Material.SILICON.ordinal()));
-        OreDictionary.registerOre("ingotSilicon", new ItemStack(EnderIO.itemMaterial, 1, Material.SILICON.ordinal()));
+        OreDictionary.registerOre("silicon", new ItemStack(ModObject.itemMaterial.getItem(), 1, Material.SILICON.ordinal()));
+        OreDictionary.registerOre("ingotSilicon", new ItemStack(ModObject.itemMaterial.getItem(), 1, Material.SILICON.ordinal()));
     }
 
     @Override
